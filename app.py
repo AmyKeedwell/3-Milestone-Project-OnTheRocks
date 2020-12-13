ab361ba063f0661ee059df50270c875e20af81a4
AmyKeedwell <amii1595@live.co.uk>
2020-12-13 13:39:05 +0000

    copied register page to create login page + added basic functionality

--- app.py
+++ app.py
@@ -56,6 +56,28 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check if user already registered
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password
+                flash("Incorrect username and/or password")
+                return redirect (url_for("login"))
+
+        else: 
+            #username doesn't exist
+            flash("Incorrect username and/or password")
+            return redirect (url_for("login"))
+
+
     return render_template("login.html")
 
 
