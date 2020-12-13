72f35140ca8c6b81b9d76e75ecde47d10eba4383
AmyKeedwell <amii1595@live.co.uk>
2020-12-13 11:43:42 +0000

    added more templates and connected app route decorators in app.py

--- app.py
+++ app.py
@@ -25,6 +25,21 @@ def get_recipes():
     return render_template("recipes.html", recipies=recipies)
 
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
