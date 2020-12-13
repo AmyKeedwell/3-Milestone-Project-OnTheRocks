876b820300fe0427a98555b9f94ca42fa77b5747
AmyKeedwell <amii1595@live.co.uk>
2020-12-13 14:36:06 +0000

    added logout function to remove session cookies and added if statement to show certain navbar options when logged in and logged out

--- app.py
+++ app.py
@@ -90,7 +90,19 @@ def profile(username):
     # get session users username from the mongo db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
